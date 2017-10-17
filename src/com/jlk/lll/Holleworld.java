3cbf641721d3a50ce1e17a65e92f8a3a9d0c973b
huwenkai <1121948633@qq.com>
2017-10-17 20:34:58 +0800

    Initial commit

---
+++ src/com/jlk/lll/Holleworld.java
@@ -0,0 +1,10 @@
+package com.jlk.lll;
+
+public class Holleworld extends Ation{
+
+
+    public String getHello() throws Exception {
+        System.out.println("com.jlk.lll.Holleworld" + name + "   " + age);
+        return "success";
+    }
+}
